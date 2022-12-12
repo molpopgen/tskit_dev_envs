458a2c9f7d256dda81768d2b44c13dc07bf1abe1
Kevin R. Thornton <krthornt@uci.edu>
2022-12-12 08:17:15 -0800

    set up pre-commit

--- fedora/bootstrap_devel.sh
+++ fedora/bootstrap_devel.sh
@@ -3,3 +3,8 @@ python3.10 -m venv venv
 . venv/bin/activate
 python -m pip install --upgrade pip setuptools clang-format==6.0.1
 python -m pip install -r tskit/python/requirements/development.txt
+# Need this for pre-commit to work
+ln -s clang-format $(which clang-format)-6.0
+cd tskit
+pre-commit install
+
--- ubuntu/bootstrap_devel.sh
+++ ubuntu/bootstrap_devel.sh
@@ -1,5 +1,10 @@
 git clone https://github.com/tskit-dev/tskit
-python3 -m venv venv
+python3.10 -m venv venv
 . venv/bin/activate
-python -m pip install --upgrade pip setuptools wheel clang-format==6.0.1
+python -m pip install --upgrade pip setuptools clang-format==6.0.1
 python -m pip install -r tskit/python/requirements/development.txt
+# Need this for pre-commit to work
+ln -s clang-format $(which clang-format)-6.0
+cd tskit
+pre-commit install
+
